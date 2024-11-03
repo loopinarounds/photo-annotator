91dc36ff8df71068405b8342221d3cb8f0492751
Brad <bradley.weaver@easyrecruit.ai>
2024-11-03 23:09:47 +0000

    feat: schema changes and proper route handling for rooms

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home";
+import { Room } from "./pages/Room";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/:roomId" element={<Room />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/Room.tsx
@@ -0,0 +1,11 @@
+import { useParams } from "react-router-dom";
+
+export function Room() {
+  const { roomId } = useParams();
+
+  return (
+    <div>
+      <h1>Room {roomId}</h1>
+    </div>
+  );
+}
